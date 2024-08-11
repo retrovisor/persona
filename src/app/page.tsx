b584985270ea749560f8cd2c2ec6285ac2da6896
retrovisor <andrenfaust@gmail.com>
2024-08-11 14:39:06 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,14 @@ export const maxDuration = 181
 
 const Page = () => {
   return (
-    <section>
-      <div className="flex flex-col md:flex-row">
-        <div className="relative flex min-h-screen flex-col justify-center bg-[#F9FAFB] p-8 sm:p-12 md:w-1/2 md:p-16 lg:p-24">
-         
+    <section className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col md:flex-row max-w-4xl w-full">
+        <div className="relative flex flex-col justify-center bg-[#F9FAFB] p-8 sm:p-12 md:p-16 lg:p-24 w-full">
           <div className="grow" />
 
           <div>
             <div>
-              <h1 className="mb-8 text-4xl md:text-5xl 2xl:text-6xl">
+              <h1 className="mb-8 text-4xl md:text-5xl 2xl:text-6xl text-center">
                 discover your <br />
                 <span
                   className="bg-clip-text text-transparent"
@@ -30,7 +29,7 @@ const Page = () => {
               </h1>
 
               <div className="mb-8 flex w-full flex-col pt-2">
-                <div className="flex w-full items-center">
+                <div className="flex w-full items-center justify-center">
                   <Suspense>
                     <NewUsernameForm />
                   </Suspense>
@@ -38,7 +37,7 @@ const Page = () => {
               </div>
             </div>
             <div className="pt-8">
-              <h1 className="mb-8 text-4xl md:text-5xl 2xl:text-6xl">
+              <h1 className="mb-8 text-4xl md:text-5xl 2xl:text-6xl text-center">
                 check your{' '}
                 <span className="inline-flex items-center align-middle">
                   <PiXLogo />
@@ -56,31 +55,28 @@ const Page = () => {
               </h1>
 
               <div className="mb-8 flex w-full flex-col pt-2">
-                <div className="flex w-full items-center">
+                <div className="flex w-full items-center justify-center">
                   <Suspense>
                     <NewPairFormBothNames />
                   </Suspense>
                 </div>
               </div>
             </div>
-
-            
           </div>
           <div className="grow" />
 
           <div className="bottom-6 space-y-3 border-t">
             <div className="flex flex-col gap-2">
-              <p className="mt-8 text-sm">
+              <p className="mt-8 text-sm text-center">
                  
               </p>
-              <div className="flex flex-wrap gap-2">
+              <div className="flex flex-wrap gap-2 justify-center">
                  
                 
               </div>
             </div>
           </div>
         </div>
-         
       </div>
       <TopList />
     </section>
