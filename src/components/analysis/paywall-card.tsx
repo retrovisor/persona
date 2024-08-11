85b202bff6af9b19f817795b9506af3aade00e8f
retrovisor <andrenfaust@gmail.com>
2024-08-11 15:20:17 +0200

    Update paywall-card.tsx

--- src/components/analysis/paywall-card.tsx
+++ src/components/analysis/paywall-card.tsx
@@ -42,7 +42,7 @@ export const PaywallCard: React.FC = () => {
     },
   })
 
-  const paywallFlag = posthog.getFeatureFlag('paywall2') ?? searchParams.get('stripe')
+  const paywallFlag = false;
 
   // console.log('paywall flag', paywallFlag, searchParams.get('stripe'))
 
